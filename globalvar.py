8bb244f3d3bcd8453f95bb072dccd07c591b80cc
shorius <76928680+shorius@users.noreply.github.com>
2022-03-13 19:58:02 +0800

    Update globalvar.py

--- globalvar.py
+++ globalvar.py
@@ -3,21 +3,21 @@
 import os
 import sys
 
-###存檔路徑
+### 存檔路徑
 path = 'D:\\PycharmProjects\\paytable v1.0\\'
 
 language = ['eng', 'sch', 'tch', 'tai']
-id = 122
-###押注設定要填小數點
+id = "GAMEID"
+### 押注設定要填小數點
 cost = 1.0
 Denom_1 = [0.2, 0.5, 5.0, 6.0, 50.0]
 Times_1 = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0]
 
-###如果沒有第二組面額跟倍數,填空值即可 Denom_2=[]
+### 如果沒有第二組面額跟倍數,填空值即可 Denom_2=[]
 Denom_2 = []
 Times_2 = []
 
-###計算總投注
+### 計算總投注
 func = lambda x, y: [round(float(x[i // len(Times_1)] * Times_1[i % len(Times_1)]), 2) for i in
                      range(len(Denom_1) * len(Times_1))]
 D1T1 = func(Denom_1, Times_2)
@@ -31,8 +31,7 @@ else:
     Bet_list = sorted(list(set(map(lambda x: round(float(x * cost), 2), D1T1))))
     print(Bet_list)
 
-# gameid = 'RKS-H5-00'+ id
-gameid = 'PSS-ON-%05d' % id
+gameid = 'XXX-XX-%05d' % id
 gamepath = path + gameid + '\\'
 
 imageSave = gamepath + 'photo'
