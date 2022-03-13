6bfaf1449f87a378c49a1a2761b5d19be5abb285
shorius <76928680+shorius@users.noreply.github.com>
2022-03-13 20:09:32 +0800

    Update globalvar.py

--- globalvar.py
+++ globalvar.py
@@ -5,13 +5,12 @@ import sys
 
 cost = 1.0
 Denom_1 = [0.2, 0.5, 5.0, 6.0, 50.0]
-Times_1 = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0, 10.0]
+Times_1 = [1.0, 2.0, 3.0, 6.0, 8.0, 10.0]
 
-### 如果沒有第二組面額跟倍數,填空值即可 Denom_2=[]
 Denom_2 = []
 Times_2 = []
 
-### 計算總投注
+# 計算總投注
 func = lambda x, y: [round(float(x[i // len(Times_1)] * Times_1[i % len(Times_1)]), 2) for i in
                      range(len(Denom_1) * len(Times_1))]
 D1T1 = func(Denom_1, Times_2)
@@ -24,12 +23,6 @@ if len(Denom_2) > 0:
 else:
     Bet_list = sorted(list(set(map(lambda x: round(float(x * cost), 2), D1T1))))
     print(Bet_list)
-
-gameid = 'XXX-XX-%05d' % id
-gamepath = path + gameid + '\\'
-
-imageSave = gamepath + 'photo'
-result = gamepath + 'payresult'
 
 
 ### 建立截圖&結果存放資料夾
